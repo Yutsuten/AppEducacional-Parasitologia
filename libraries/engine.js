6f06478812b02517b6c1fa2991a5fd3dadb1a842
mateus-etto <mateus.etto@gmail.com>
2016-10-06 17:39:14 -0300

    separado em outro script o setup do pixi e seus aliases

--- libraries/engine.js
+++ libraries/engine.js
@@ -1,28 +1,3 @@
-console.log("Pixi.js version: " + PIXI.VERSION);
-
-// Aliases
-var Container = PIXI.Container,
-    autoDetectRenderer = PIXI.autoDetectRenderer,
-    loader = PIXI.loader,
-    resources = PIXI.loader.resources,
-    Sprite = PIXI.Sprite,
-    TextureCache = PIXI.utils.TextureCache,
-    Rectangle = PIXI.Rectangle,
-    Graphics = PIXI.Graphics,
-    Text = PIXI.Text;
-
-// Renderer and Auto-scaling options
-var renderer = autoDetectRenderer(1920, 1080);
-renderer.autoResize = true;
-document.body.appendChild(renderer.view);
-var scale = scaleToWindow(renderer.view);
-window.addEventListener("resize", function(event) {
-  scale = scaleToWindow(renderer.view);
-});
-renderer.view.style.position = "absolute";
-renderer.view.style.display = "block";
-var stage = new Container();
-
 // Scene array to be filled on index.js
 var scene = [];
 
