bc99f99e319d305068e3eeece775cf97e2bfb742
mateus-etto <mateus.etto@gmail.com>
2016-10-08 20:07:47 -0300

    zoom with image working

--- libraries/engine.js
+++ libraries/engine.js
@@ -68,12 +68,11 @@ function loadSpritesFromTextures() {
     scene[i].backgroundImage = new PIXI.Sprite(resources[scene[i].backgroundImage].texture);
     scene[i].backgroundImage.interactive = true;
     scene[i].backgroundImage.click = function(evt) {
-      zoomTriangle.visible = false;
+      HideZoom();
       if (showCoordinates) {
         var mousePosition = evt.data.getLocalPosition(stage);
         console.log(Math.round(mousePosition.x) + "; " + Math.round(mousePosition.y));
       }
-      UpdateScreen();
     };
 
     for (var j = 0; j < scene[i].item.length; j++) {
@@ -88,6 +87,9 @@ function loadSpritesFromTextures() {
       }
       if (scene[i].item[j].zoomImage) {
         scene[i].item[j].zoomImage = new PIXI.Sprite(resources[scene[i].item[j].zoomImage].texture);
+        scene[i].item[j].zoomImage.anchor.set(0.5, 0.5);
+        scene[i].item[j].zoomImage.height = 2*zoomSize;
+        scene[i].item[j].zoomImage.width = 2*zoomSize;
         scene[i].item[j].zoomImage.visible = false;
         AddClickShowZoomEvent(scene[i].item[j]);
       }
@@ -97,13 +99,22 @@ function loadSpritesFromTextures() {
   UpdateScreen();
 }
 
+function HideZoom() {
+  zoomTriangle.visible = false;
+  for (var i = 0; i < scene.length; i++)
+    for (var j = 0; j < scene[i].item.length; j++)
+      if (scene[i].item[j].zoomImage)
+        scene[i].item[j].zoomImage.visible = false;
+  UpdateScreen();
+}
+
 function CreateTriangle(size) {
   var triangle = new Graphics();
   triangle.beginFill(0x000000);
   triangle.drawPolygon([
-      -size, 4*size,             //First point
-      size, 4*size,              //Second point
-      0, 0                 //Third point
+      -size, 4*size,
+      size, 4*size,
+      0, 0
   ]);
   triangle.endFill();
   triangle.visible = false;
@@ -130,7 +141,7 @@ function AddClickChangeSceneEvent(item) {
   item.image.click = function(interaction) {
     /*var audio = new Audio("audio/explosion.wav");
     audio.play();*/
-    zoomTriangle.visible = false;
+    HideZoom();
     activeScene = item.sceneChange;
     UpdateScreen();
   };
@@ -139,17 +150,27 @@ function AddClickChangeSceneEvent(item) {
 function AddClickShowZoomEvent(item) {
   AddEvent(item);
   item.image.click = function(evt) {
-    console.log("clicked on object");
+    // Hide any other zoom being display
+    HideZoom();
+
+    // Show zoom of the item clicked
     zoomTriangle.x = evt.target.position.x;
     zoomTriangle.y = evt.target.position.y;
     if (zoomTriangle.x < renderer.width/2) { // Left side
       // Rotate triangle's base to right
-      zoomTriangle.rotation = 1.5 * Math.PI; //4*zoomSize
-
+      zoomTriangle.rotation = 1.5 * Math.PI;
+      // Show the zoom image
+      item.zoomImage.x = item.position.x + 4 * zoomSize;
+      item.zoomImage.y = item.position.y;
+      item.zoomImage.visible = true;
     }
     else { // Right side
       // Rotate triangle's base to left
       zoomTriangle.rotation = 0.5 * Math.PI;
+      // Show the zoom image
+      item.zoomImage.x = item.position.x - 4 * zoomSize;
+      item.zoomImage.y = item.position.y;
+      item.zoomImage.visible = true;
     }
     zoomTriangle.visible = true;
     UpdateScreen();
@@ -157,14 +178,24 @@ function AddClickShowZoomEvent(item) {
 }
 
 function UpdateScreen() {
+  // Draw Background
   stage.addChild(scene[activeScene].backgroundImage);
+
+  // Draw Items
   for (var i = 0; i < scene[activeScene].item.length; i++) {
     stage.addChild(scene[activeScene].item[i].image);
+  }
+
+  // Draw zoom's Triangle
+  stage.addChild(zoomTriangle);
+
+  // Draw zoom's Image
+  for (var i = 0; i < scene[activeScene].item.length; i++) {
     if (scene[activeScene].item[i].zoomImage)
       stage.addChild(scene[activeScene].item[i].zoomImage);
   }
-  stage.addChild(zoomTriangle);
 
+  // Render 'em all
   renderer.render(stage);
 }
 
