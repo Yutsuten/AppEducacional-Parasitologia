2cffa07a13be18d8da0ac829b398b0d0531cabec
mateus-etto <mateus.etto@gmail.com>
2016-10-08 22:32:01 -0300

    small code improvements

--- libraries/engine.js
+++ libraries/engine.js
@@ -162,12 +162,12 @@ function AddEvent(item) {
   item.image.interactive = true;
   // Setting glow area to be 25 pixels bigger than the image bounds
   item.image.filterArea = new Rectangle(item.image.x - item.image.width/2 - 25, item.image.y - item.image.height/2 - 25, item.image.width + 50, item.image.height + 50);
-  item.image.mouseover = function(interaction) {
-    // viewWidth, viewHeight, outerStrength, innerStrength, color, quality
+  item.image.mouseover = function(evt) {
+    // viewWidth, viewHeight, outerStrength, innerStrength, ??, color, quality
     item.image.filters = [new PIXI.filters.GlowFilter(renderer.width, renderer.height, 18, 2, 1, 0xFFFFFF, 0.3)];
     UpdateScreen();
   };
-  item.image.mouseout = function(interaction) {
+  item.image.mouseout = function(evt) {
     item.image.filters = null;
     UpdateScreen();
   };
@@ -175,7 +175,7 @@ function AddEvent(item) {
 
 function AddClickChangeSceneEvent(item) {
   AddEvent(item);
-  item.image.click = function(interaction) {
+  item.image.click = function(evt) {
     /*var audio = new Audio("audio/explosion.wav");
     audio.play();*/
     HideZoom();
@@ -191,22 +191,22 @@ function AddClickShowZoomEvent(item) {
     HideZoom();
 
     // Show zoom of the item clicked
-    zoomTriangle.x = item.zoomOffsetX + evt.target.position.x;
-    zoomTriangle.y = item.zoomOffsetY + evt.target.position.y;
+    zoomTriangle.x = item.position.x + item.zoomOffsetX;
+    zoomTriangle.y = item.position.y + item.zoomOffsetY;
     if (zoomTriangle.x < renderer.width/2) { // Left side
       // Rotate triangle's base to right
       zoomTriangle.rotation = 1.5 * Math.PI;
       // Show the zoom image
-      item.zoomImage.x = item.zoomOffsetX + item.position.x + 4 * zoomSize;
-      item.zoomImage.y = item.zoomOffsetY + item.position.y;
+      item.zoomImage.x = item.position.x + 4 * zoomSize + item.zoomOffsetX;
+      item.zoomImage.y = item.position.y + item.zoomOffsetY;
       item.zoomImage.visible = true;
     }
     else { // Right side
       // Rotate triangle's base to left
       zoomTriangle.rotation = 0.5 * Math.PI;
       // Show the zoom image
-      item.zoomImage.x = item.zoomOffsetX + item.position.x - 4 * zoomSize;
-      item.zoomImage.y = item.zoomOffsetY + item.position.y;
+      item.zoomImage.x = item.position.x - 4 * zoomSize + item.zoomOffsetX;
+      item.zoomImage.y = item.position.y + item.zoomOffsetY;
       item.zoomImage.visible = true;
     }
     zoomTriangle.visible = true;
@@ -228,7 +228,7 @@ function UpdateScreen() {
 
   // Draw zoom's Image
   for (var i = 0; i < scene[activeScene].item.length; i++) {
-    if (scene[activeScene].item[i].zoomImage)
+    if (scene[activeScene].item[i].zoomImage != null)
       stage.addChild(scene[activeScene].item[i].zoomImage);
   }
 
