98ef416afeb8bad5be2ded403edb5d05fa57f63e
mateus-etto <mateus.etto@gmail.com>
2016-10-25 16:58:04 -0200

    achievement unlocked: the ballad

--- index.js
+++ index.js
@@ -7,9 +7,30 @@ var scene1images = [
 ];
 loadImages(scene1images, scene1setup);
 
+var background;
 function scene1setup() {
-  var background = new GameImage(resources["img/classroom.jpg"].texture);
-  background.setDarkness(10);
+  background = new GameImage(resources["img/classroom.jpg"].texture);
+  var darkValue = 0;
+  var inc = true;
+  setInterval(function() {
+    if (inc) {
+      if (darkValue >= 15) {
+        darkValue -= 1;
+        inc = false;
+      }
+      darkValue += 1;
+    }
+    else {
+      if (darkValue <= 0) {
+        darkValue += 1;
+        inc = true;
+      }
+      darkValue -= 1;
+    }
+    background.setDarkness(darkValue);
+    stage.addChild(background);
+    renderer.render(stage);
+  }, 50);
   stage.addChild(background);
   renderer.render(stage);
 }
