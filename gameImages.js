ce0a954b8d9da8dd01af560ac758fc9a918bfa0d
Mateus Etto <mateus.etto@gmail.com>
2016-11-29 00:50:59 -0200

    reorganization of game images list (on a separated script)

---
+++ gameImages.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var gameImages = [
+  "img/classroom.jpg",
+  "img/classroom2.jpg",
+  "img/student.png",
+  "img/banana.png",
+  "img/close.png",
+  "img/shit.png",
+  "img/digestive-system.png",
+  "img/mosca.gif",
+  "img/arrow.png",
+  "img/sick-person.png",
+  "img/farm.png",
+  "img/callout.png"
+];
