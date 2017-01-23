43e9b9f017b588203b8ab17f338663da3a9ef0f2
Mateus Etto <mateus.etto@gmail.com>
2017-01-22 22:18:33 -0200

    initial part of Entamoeba's cycle

--- js/cena0_salaDeAula.js
+++ js/cena0_salaDeAula.js
@@ -21,22 +21,44 @@ sceneLoader[0] = function() {
 
   // Adicionando o aluno da banana
   var estudanteBananaNormal = new Game.Item("estudanteNormal.png");
-  estudanteBananaNormal.setPosition(955, 655);
+  estudanteBananaNormal.setPosition(960, 655);
 
   // Adiconando a banana
   var banana = new Game.Item("banana.png");
-  banana.setPosition(955, 760);
+  banana.setPosition(960, 760);
   banana.setScale(0.2);
   banana.setZorder(4);
   banana.addGlowEffect();
   banana.click = function(mouse) {
+    // Eventos que acontecem imediatamente apos clicar na banana
     disableInteractiveness();
-    fundoSalaDeAula.changeBrightness(50, 600);
+    fundoSalaDeAula.changeBrightness(100, 600);
+    setaJanela.fadeout(600);
+    setaArmario.fadeout(600);
+
+    // Eventos que acontecem apos 0.8 segundos do clique
+    setTimeout(function() {
+      setaJanela.disable();
+      setaArmario.disable();
+      scene[0].setBackgroundMaskColor(120, 220, 255);
+      scene[0].changeBackgroundMaskAlpha(1, 600);
+      estudanteBananaNormal.fadeout(600);
+      banana.fadeout(600);
+      sistemaDigestivo.enable();
+      sistemaDigestivo.fadein(600);
+    }, 800);
 
     setTimeout(function() {
       enableInteractiveness();
     }, 1300);
   }
+
+  // Adiciona a imagem do sistema digestivo - Ciclo Entamoeba Coli
+  var sistemaDigestivo = new Game.Item("sistemaDigestivo.png");
+  sistemaDigestivo.setPosition(960, 550);
+  sistemaDigestivo.setScale(1.3);
+  sistemaDigestivo.setAlpha(0);
+  sistemaDigestivo.disable();
 
   // Adiciona uma seta para levar a cena externa
   var setaJanela = new Game.Item("seta.png");
@@ -65,5 +87,6 @@ sceneLoader[0] = function() {
   scene[0].addItem(setaArmario);
   scene[0].addItem(estudanteBananaNormal);
   scene[0].addItem(banana);
+  scene[0].addItem(sistemaDigestivo);
 
 }
