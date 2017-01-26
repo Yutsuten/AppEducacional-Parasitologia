e18eab49308448c7cfb37d894e161524434985fa
Mateus Etto <mateus.etto@gmail.com>
2017-01-26 18:25:49 -0200

    scene 2 back to life

--- js/cena2_armario.js
+++ js/cena2_armario.js
@@ -6,38 +6,41 @@ sceneLoader[2] = function() {
   scene[2] = new Game.Scene();
   // Adicionando musica na cena
   scene[2].setMusic("musicaSalaDeAula.mp3");
-/*
+
   // Adicionando uma imagem de fundo
-  //var fundoCenaArmario = new Game.Background("cenaArmarioBackground.jpg");
+  scene[2].background = new Game.Background("cenaArmarioBackground.jpg");
   // Para ajuda no desenvolvimento, colocando um evento de clique no fundo para
   // Mostrar as coordenadas
   var mostraCoordenadasAoClicar = true;
-  fundoCenaArmario.click = function(mouse) {
+  scene[2].background.click = function(mouse) {
     if (mostraCoordenadasAoClicar) {
       var mousePosition = mouse.data.getLocalPosition(stage);
       console.log(Math.round(mousePosition.x) + "; " + Math.round(mousePosition.y));
     }
   }
 
+  // Criando um aliase de scene[2].item
+  var item = scene[2].item;
+
   // Adiciona a imagem do armario
-  var armarioDeLivros = new Game.Item("armarioLivros.jpg");
-  armarioDeLivros.setPosition(990, 560);
-  armarioDeLivros.setScale(0.8);
+  item["armarioDeLivros"] = new Game.Item("armarioLivros.jpg");
+  item.armarioDeLivros.setPosition(990, 560);
+  item.armarioDeLivros.setScale(0.8);
 
   // Adiciona uma seta para voltar na sala de aula
-  var setaSalaDeAula = new Game.Item("seta.png");
-  setaSalaDeAula.setPosition(1735, 480);
-  setaSalaDeAula.setScale(0.5);
-  setaSalaDeAula.addGlowEffect();
-  setaSalaDeAula.click = function(mouse) {
+  item["setaSalaDeAula"] = new Game.Item("seta.png");
+  item.setaSalaDeAula.setPosition(1735, 480);
+  item.setaSalaDeAula.setScale(0.5);
+  item.setaSalaDeAula.onMouseOver = function() {
+    item.setaSalaDeAula.addGlow();
+  }
+  item.setaSalaDeAula.onMouseOut = function() {
+    item.setaSalaDeAula.removeGlow();
+  }
+  item.setaSalaDeAula.click = function(mouse) {
     changeScene(0); // Muda para a cena exterior
   }
 
-  // Adicionando o Background e os Itens na cena
-  // Background
-  //scene[2].addBackground(fundoCenaArmario);
-  // Itens
-  scene[2].addItem(armarioDeLivros);
-  scene[2].addItem(setaSalaDeAula);*/
+  scene[2].addAllItemsToScene();
 
 }
