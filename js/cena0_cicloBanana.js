136f213f758c23f830dc659df0ec4512c6ffba9d
Mateus Etto <mateus.etto@gmail.com>
2017-02-10 01:17:51 -0200

    run methods in groups (array) is working

--- js/cena0_cicloBanana.js
+++ js/cena0_cicloBanana.js
@@ -110,6 +110,8 @@ function carregaCicloBanana() {
     }, 7600);
   }
 
+  var itensCiclo = [];
+
   // Adiciona a imagem do sistema digestivo - Ciclo Entamoeba Coli
   item["sistemaDigestivo"] = new Game.Image("sistemaDigestivo.png");
   item.sistemaDigestivo.setPosition(960, 550);
@@ -118,6 +120,7 @@ function carregaCicloBanana() {
   item.sistemaDigestivo.setInteractive(false);
   item.sistemaDigestivo.setAlpha(0);
   item.sistemaDigestivo.disable();
+  itensCiclo.push(item.sistemaDigestivo);
 
   // Adiciona a primeira imagem do ciclo Entamoeba Coli
   item["bananaInfectada"] = new Game.Image("temp.jpg");
@@ -125,6 +128,7 @@ function carregaCicloBanana() {
   item.bananaInfectada.setZorder(4);
   item.bananaInfectada.setAlpha(0);
   item.bananaInfectada.disable();
+  itensCiclo.push(item.bananaInfectada);
 
   item["zoomBanana"] = new Game.Line(355, 255, 1030, 280, 3);
   item.zoomBanana.setColor(0, 0, 0);
@@ -132,6 +136,7 @@ function carregaCicloBanana() {
   item.zoomBanana.addArrowAtEnd();
   item.zoomBanana.setAlpha(0);
   item.zoomBanana.disable();
+  itensCiclo.push(item.zoomBanana);
 
   // Adiciona a segunda imagem do ciclo Entamoeba Coli
   item["coliDesencistamento"] = new Game.Image("temp.jpg");
@@ -139,6 +144,7 @@ function carregaCicloBanana() {
   item.coliDesencistamento.setZorder(4);
   item.coliDesencistamento.setAlpha(0);
   item.coliDesencistamento.disable();
+  itensCiclo.push(item.coliDesencistamento);
 
   // Adiciona a terceira imagem do ciclo Entamoeba Coli
   item["coliEncistamento"] = new Game.Image("temp.jpg");
@@ -147,6 +153,7 @@ function carregaCicloBanana() {
   item.coliEncistamento.setZorder(4);
   item.coliEncistamento.setAlpha(0);
   item.coliEncistamento.disable();
+  itensCiclo.push(item.coliEncistamento);
 
   // Adiciona a quarta imagem do ciclo Entamoeba Coli
   item["coliFezes"] = new Game.Image("fezes.png");
@@ -155,6 +162,7 @@ function carregaCicloBanana() {
   item.coliFezes.setZorder(4);
   item.coliFezes.setAlpha(0);
   item.coliFezes.disable();
+  itensCiclo.push(item.coliFezes);
 
   // Adiciona a quinta imagem do ciclo Entamoeba Coli
   item["coliMosca"] = new Game.Image("temp.jpg");
@@ -163,12 +171,13 @@ function carregaCicloBanana() {
   item.coliMosca.setZorder(4);
   item.coliMosca.setAlpha(0);
   item.coliMosca.disable();
+  itensCiclo.push(item.coliMosca);
 
   item["coliBotaoFechar"] = new Game.Item("botaoFechar.png");
   item.coliBotaoFechar.setPosition(1850, 78);
   item.coliBotaoFechar.setAlpha(0);
   item.coliBotaoFechar.disable();
-  item.coliBotaoFechar.setInteractive(true);
+  itensCiclo.push(item.coliBotaoFechar);
   item.coliBotaoFechar.onMouseOver = function() {
     item.coliBotaoFechar.addGlow();
   }
@@ -179,61 +188,19 @@ function carregaCicloBanana() {
     disableInteractiveness();
 
     // Fadeout em todas as imagens do ciclo
-    item.sistemaDigestivo.changeAlpha(0, 600);
-    item.bananaInfectada.changeAlpha(0, 600);
-    item.zoomBanana.changeAlpha(0, 600);
-    item.coliDesencistamento.changeAlpha(0, 600);
-    item.coliEncistamento.changeAlpha(0, 600);
-    item.coliFezes.changeAlpha(0, 600);
-    item.coliMosca.changeAlpha(0, 600);
-    item.coliBotaoFechar.changeAlpha(0, 600);
+    groupChangeAlpha(itensCiclo, 0, 600);
 
     // Eventos que acontecem apos 0.8 segundos do clique
     setTimeout(function() {
       // Desabilita todos os itens do ciclo
-      item.sistemaDigestivo.disable();
-      item.bananaInfectada.disable();
-      item.zoomBanana.disable();
-      item.coliDesencistamento.disable();
-      item.coliEncistamento.disable();
-      item.coliFezes.disable();
-      item.coliMosca.disable();
-      item.coliBotaoFechar.disable();
+      groupDisable(itensCiclo);
 
       // Mostra o estudante doente
-      item.estudanteDoente.enable();
-      item.estudanteDoente.changeAlpha(1, 600);
-
       // Mostra o balao e as informacoes
-      item.balaoSobreColi.enable();
-      item.balaoSobreColi.changeAlpha(1, 600);
-
-      item.balaoAbaSintomas.enable();
-      item.balaoAbaSintomas.changeAlpha(1, 600);
-
-      item.balaoAbaSintomasTexto.enable();
-      item.balaoAbaSintomasTexto.changeAlpha(1, 600);
-
-      item.balaoAbaTratamento.enable();
-      item.balaoAbaTratamento.changeAlpha(1, 600);
-
-      item.balaoAbaTratamentoTexto.enable();
-      item.balaoAbaTratamentoTexto.changeAlpha(1, 600);
-
-      item.balaoAbaTransmissao.enable();
-      item.balaoAbaTransmissao.changeAlpha(1, 600);
-
-      item.balaoAbaTransmissaoTexto.enable();
-      item.balaoAbaTransmissaoTexto.changeAlpha(1, 600);
-
-      item.balaoDescricaoSintomas.enable();
-      item.balaoDescricaoSintomas.changeAlpha(1, 600);
-
-      item.botaoFinalizar.enable();
-      item.botaoFinalizar.changeAlpha(1, 600);
-
-      item.botaoFinalizarTexto.enable();
-      item.botaoFinalizarTexto.changeAlpha(1, 600);
+      groupEnable(itensSobreDoenca);
+      groupChangeAlpha(itensSobreDoenca, 1, 600);
+      item.balaoDescricaoTratamento.disable();
+      item.balaoDescricaoTransmissao.disable();
     }, 800);
 
     setTimeout(function() {
@@ -241,12 +208,15 @@ function carregaCicloBanana() {
     }, 1600);
   }
 
+  var itensSobreDoenca = [];
+
   item["estudanteDoente"] = new Game.Image("estudanteDoente.png");
   item.estudanteDoente.setPosition(600, 660);
   item.estudanteDoente.setZorder(11);
   item.estudanteDoente.setScale(1.3);
   item.estudanteDoente.setAlpha(0);
   item.estudanteDoente.disable();
+  itensSobreDoenca.push(item.estudanteDoente);
 
   item["balaoSobreColi"] = new Game.Image("balao.png");
   item.balaoSobreColi.setPosition(1270, 260);
@@ -254,6 +224,7 @@ function carregaCicloBanana() {
   item.balaoSobreColi.setScale(1.4);
   item.balaoSobreColi.setAlpha(0);
   item.balaoSobreColi.disable();
+  itensSobreDoenca.push(item.balaoSobreColi);
 
   // Aba Sintomas
   item["balaoAbaSintomas"] = new Game.Item("abaDoBalao.png");
@@ -269,6 +240,7 @@ function carregaCicloBanana() {
     item.balaoDescricaoSintomas.enable();
     UpdateScreen();
   }
+  itensSobreDoenca.push(item.balaoAbaSintomas);
 
   item["balaoAbaSintomasTexto"] = new Game.Text("Sintomas", {font: "30px Arial", fill: 0x000000, align: "left"});
   item.balaoAbaSintomasTexto.setPosition(950, 80);
@@ -276,12 +248,14 @@ function carregaCicloBanana() {
   item.balaoAbaSintomasTexto.setAnchor(0.5, 0.5);
   item.balaoAbaSintomasTexto.setAlpha(0);
   item.balaoAbaSintomasTexto.disable();
+  itensSobreDoenca.push(item.balaoAbaSintomasTexto);
 
   item["balaoDescricaoSintomas"] = new Game.Text("Descrição sintomas ...", {font: "32px Arial", fill: 0x000000, align: "left"});
   item.balaoDescricaoSintomas.setPosition(810, 140);
   item.balaoDescricaoSintomas.setZorder(1);
   item.balaoDescricaoSintomas.setAlpha(0);
   item.balaoDescricaoSintomas.disable();
+  itensSobreDoenca.push(item.balaoDescricaoSintomas);
 
   // Aba Tratamento
   item["balaoAbaTratamento"] = new Game.Item("abaDoBalao.png");
@@ -297,6 +271,7 @@ function carregaCicloBanana() {
     item.balaoDescricaoTratamento.enable();
     UpdateScreen();
   }
+  itensSobreDoenca.push(item.balaoAbaTratamento);
 
   item["balaoAbaTratamentoTexto"] = new Game.Text("Tratamento", {font: "30px Arial", fill: 0x000000, align: "left"});
   item.balaoAbaTratamentoTexto.setPosition(1270, 80);
@@ -304,11 +279,13 @@ function carregaCicloBanana() {
   item.balaoAbaTratamentoTexto.setAnchor(0.5, 0.5);
   item.balaoAbaTratamentoTexto.setAlpha(0);
   item.balaoAbaTratamentoTexto.disable();
+  itensSobreDoenca.push(item.balaoAbaTratamentoTexto);
 
   item["balaoDescricaoTratamento"] = new Game.Text("Descrição tratamento ...", {font: "32px Arial", fill: 0x000000, align: "left"});
   item.balaoDescricaoTratamento.setPosition(810, 140);
   item.balaoDescricaoTratamento.setZorder(1);
   item.balaoDescricaoTratamento.disable();
+  itensSobreDoenca.push(item.balaoDescricaoTratamento);
 
   // Aba Transmissao
   item["balaoAbaTransmissao"] = new Game.Item("abaDoBalao.png");
@@ -324,6 +301,7 @@ function carregaCicloBanana() {
     item.balaoDescricaoTransmissao.enable();
     UpdateScreen();
   }
+  itensSobreDoenca.push(item.balaoAbaTransmissao);
 
   item["balaoAbaTransmissaoTexto"] = new Game.Text("Transmissão", {font: "30px Arial", fill: 0x000000, align: "left"});
   item.balaoAbaTransmissaoTexto.setPosition(1590, 80);
@@ -331,11 +309,13 @@ function carregaCicloBanana() {
   item.balaoAbaTransmissaoTexto.setAnchor(0.5, 0.5);
   item.balaoAbaTransmissaoTexto.setAlpha(0);
   item.balaoAbaTransmissaoTexto.disable();
+  itensSobreDoenca.push(item.balaoAbaTransmissaoTexto);
 
   item["balaoDescricaoTransmissao"] = new Game.Text("Descrição transmissão ...", {font: "32px Arial", fill: 0x000000, align: "left"});
   item.balaoDescricaoTransmissao.setPosition(810, 140);
   item.balaoDescricaoTransmissao.setZorder(1);
   item.balaoDescricaoTransmissao.disable();
+  itensSobreDoenca.push(item.balaoDescricaoTransmissao);
 
   // Botao OK, entendi
   item["botaoFinalizar"] = new Game.Item("abaDoBalao.png");
@@ -344,44 +324,16 @@ function carregaCicloBanana() {
   item.botaoFinalizar.setScale(1.3);
   item.botaoFinalizar.setAlpha(0);
   item.botaoFinalizar.disable();
-  item.botaoFinalizar.setInteractive(true);
   item.botaoFinalizar.click = function(mouse) {
     disableInteractiveness();
 
     // Oculta o estudante doente
-    item.estudanteDoente.changeAlpha(0, 600);
     // Oculta o balao e as informacoes
-    item.balaoSobreColi.changeAlpha(0, 600);
-    item.balaoAbaSintomas.changeAlpha(0, 600);
-    item.balaoAbaSintomasTexto.changeAlpha(0, 600);
-    item.balaoAbaTratamento.changeAlpha(0, 600);
-    item.balaoAbaTratamentoTexto.changeAlpha(0, 600);
-    item.balaoAbaTransmissao.changeAlpha(0, 600);
-    item.balaoAbaTransmissaoTexto.changeAlpha(0, 600);
-    item.balaoDescricaoSintomas.changeAlpha(0, 600);
-    item.botaoFinalizar.changeAlpha(0, 600);
-    item.botaoFinalizarTexto.changeAlpha(0, 600);
-    item.balaoDescricaoSintomas.changeAlpha(0, 600);
-    item.balaoDescricaoTratamento.changeAlpha(0, 600);
-    item.balaoDescricaoTransmissao.changeAlpha(0, 600);
+    groupChangeAlpha(itensSobreDoenca, 0, 600);
 
     setTimeout(function() {
       // Desabilita as imagens que desapareceram
-      item.estudanteDoente.disable();
-      item.balaoSobreColi.disable();
-      item.balaoAbaSintomas.disable();
-      item.balaoAbaSintomasTexto.disable();
-      item.balaoAbaTratamento.disable();
-      item.balaoAbaTratamentoTexto.disable();
-      item.balaoAbaTransmissao.disable();
-      item.balaoAbaTransmissaoTexto.disable();
-      item.balaoDescricaoSintomas.disable();
-      item.botaoFinalizar.disable();
-      item.botaoFinalizarTexto.disable();
-
-      item.balaoDescricaoSintomas.disable();
-      item.balaoDescricaoTratamento.disable();
-      item.balaoDescricaoTransmissao.disable();
+      groupDisable(itensSobreDoenca);
 
       // Volta a cena inicial
       item.estudanteBananaNormal.enable();
@@ -405,6 +357,7 @@ function carregaCicloBanana() {
       enableInteractiveness();
     }, 1600);
   }
+  itensSobreDoenca.push(item.botaoFinalizar);
 
   item["botaoFinalizarTexto"] = new Game.Text("OK, entendi!", {font: "30px Arial", fill: 0x000000, align: "left"});
   item.botaoFinalizarTexto.setPosition(1600, 320);
@@ -412,5 +365,6 @@ function carregaCicloBanana() {
   item.botaoFinalizarTexto.setAnchor(0.5, 0.5);
   item.botaoFinalizarTexto.setAlpha(0);
   item.botaoFinalizarTexto.disable();
+  itensSobreDoenca.push(item.botaoFinalizarTexto);
 
 }
