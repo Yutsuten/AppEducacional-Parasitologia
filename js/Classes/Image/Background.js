92d980f1e3a6711b4b2427fb02a06e47158e5d1f
Mateus Etto <mateus.etto@gmail.com>
2017-02-11 20:44:57 -0200

    removed access to a lot of methods of Background class

--- js/Classes/Image/Background.js
+++ js/Classes/Image/Background.js
@@ -7,6 +7,30 @@ Game.Background = function(texture) {
   this.interactive = false;
   this.width = renderer.width;
   this.height = renderer.height;
+
+  // PROPERTIES
+  // Public properties
+  this.z_order = null;
+
+  // METHODS
+  // Public methods
+  // Disable methods that must not be called
+  // Set value methods (changes instantly)
+  this.setPosition = null;
+  this.setScale = null;
+  this.setZorder = null;
+  this.setRotation = null;
+  this.setHeight = null;
+  this.setWidth = null;
+  this.setAnchor = null;
+
+  // Change value methods (animations that change properties within time)
+  this.changeScale = null;
+  this.move = null;
+  this.addGlow = null;
+  this.removeGlow = null;
+  this.disable = null;
+  this.enable = null;
 }
 
 Game.Background.prototype = Object.create(Game.Image.prototype);
