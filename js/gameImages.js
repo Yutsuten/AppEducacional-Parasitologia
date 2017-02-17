a7a928aea2ec1a0cef66c760ebb8da2ff9166783
Mateus Etto <mateus.etto@gmail.com>
2017-02-17 21:44:53 -0200

    updated images to be used at gameImages.js

--- js/gameImages.js
+++ js/gameImages.js
@@ -3,23 +3,12 @@
 var gameImages = [
   // Background Cenas
   "salaDeAula.jpg",
-  "cenaExterior.png",
-  "cenaArmarioBackground.jpg",
+  "cenaExterior.jpg",
+  "armario.jpg",
 
   // Imagens Sala de Aula
-  "estudanteNormal.png",
   "salaDeAula_carteiras.png",
   "salaDeAula_mesaProfessor.png",
-  "banana.png",
-
-  // Imagens ciclo da banana (Entamoeba Coli)
-  "estudanteDoente.png",
-  "fezes.png",
-  "entamoebaCisto.png",
-  "entamoebaTrofozoito.png",
-  "sistemaDigestivo.png",
-  "mosca.gif",
-  "ovos.png",
 
   // Imagens Cena Exterior
   "chiqueiro.png",
@@ -29,14 +18,86 @@ var gameImages = [
   "poco.png",
   "vaca.png",
 
-  // Imagens Armario
-  "armarioLivros.jpg",
+  // Estudantes
+  "estHbrcClo.png",
+  "estHbrcDte.png",
+  "estHbrcSdv.png",
+  "estHjpnClo.png",
+  "estHjpnDte.png",
+  "estHjpnSdv.png",
+  "estHngrClo.png",
+  "estHngrDte.png",
+  "estHngrSdv.png",
+  "estHprdClo.png",
+  "estHprdDte.png",
+  "estHprdSdv.png",
+  "estMbrcClo.png",
+  "estMbrcDte.png",
+  "estMbrcSdv.png",
+  "estMjpnClo.png",
+  "estMjpnDte.png",
+  "estMjpnSdv.png",
+  "estMngrClo.png",
+  "estMngrDte.png",
+  "estMngrSdv.png",
+  "estMprdClo.png",
+  "estMprdDte.png",
+  "estMprdSdv.png",
+
+  // Ciclo do Entamoeba Coli (banana)
+  "fezesComMosca.png",
+  "entamoebaCisto.png",
+  "entamoebaTrofozoito.png",
+
+  // Ciclo do Ascaris (tomate)
+  "circulacao.png",
+  "pulmao.png",
+  "coracao.png",
+  "figado.png",
+  "fezesSemMosca.png",
+
+  // Ciclo do Plasmodium (pernilongo)
+
+  // Ciclo do Toxoplasmose (gato)
+
+  // Ciclo da Giardia (poco)
+  "poco.png",
+  "cistos.png",
+  "giardiaReproducao.png",
+  "maisCistos.png",
+  "lencolFreatico.png",
+
+  // Ciclo da Taenia Saginata (vaca)
+  "vaca.png",
+  "carneInfectada.png",
+  "teniaAdulta.png",
+  "proglate.png",
+  "papelLixo.png",
+
+  // Ciclo da Taenia Solium (porco)
+  "porco.png",
+
+  // Ciclo da Cisticercose (cenoura)
+  "chorumeLixoCenouras.png",
+  "cerebroInfectado.png",
+
+  // Balao sobre doenca
+  "balTranEsq.png",
+  "balTranDir.png",
+  "balSintEsq.png",
+  "balSintDir.png",
+  "balPrevEsq.png",
+  "balPrevDir.png",
+  "abaTranHover.png",
+  "abaTran.png",
+  "abaSintHover.png",
+  "abaSint.png",
+  "abaPrevHover.png",
+  "abaPrev.png",
+  "okNormal.png",
+  "okHover.png",
 
   // Imagens Gerais (usadas em varios lugares)
   "botaoFechar.png",
-  "balao.png",
-  "abaDoBalao.png",
-  "botao.png",
-  "temp.jpg",
   "seta.png"
 ];
