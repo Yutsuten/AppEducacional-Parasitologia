70caf3a9541eb3efa789b434ff806b09c8d88007
Mateus Etto <mateus.etto@gmail.com>
2017-02-18 23:14:49 -0200

    updated subtitle font

--- js/Classes/Scene.js
+++ js/Classes/Scene.js
@@ -15,7 +15,7 @@ Game.Scene = function() {
   var backgroundMask = new Game.Rectangle(renderer.view.width, renderer.view.height);
   var subtitle = new MultiStyleText("",
     {
-      "default": {fontFamily: "SimSun", fontSize: "60px",
+      "default": {fontFamily: "Book Antiqua", fontSize: "60px",
         //stroke: 'black', strokeThickness: 10,
         dropShadow: true, dropShadowBlur: 10,
         fill: 0xFFFFFF, align: "left"
