12e6389e10481d7266bbb6b7e6d9c1b826097aed
Mateus Etto <mateus.etto@gmail.com>
2017-02-18 22:51:48 -0200

    added subtitles to all elements of E. coli cycle

--- js/cena0_cicloEcoli.js
+++ js/cena0_cicloEcoli.js
@@ -66,12 +66,18 @@ function carregaCicloEcoli() {
   //item.estBananaCiclo.disable();
   itensCiclo.push(item.estBananaCiclo);
 
-  item["bananaElem1"] = new Game.Image("entamoebaCisto.png");
+  item["bananaElem1"] = new Game.Item("entamoebaCisto.png");
   item.bananaElem1.setPosition(485, 300);
   item.bananaElem1.setScale(0.7);
   item.bananaElem1.setZorder(6);
   //item.bananaElem1.setAlpha(0);
   //item.bananaElem1.disable();
+  item.bananaElem1.onMouseOver = function() {
+    scene[0].setSubtitle("Cisto de <i>Entamoeba coli</i>");
+  }
+  item.bananaElem1.onMouseOut = function() {
+    scene[0].setSubtitle("");
+  }
   itensCiclo.push(item.bananaElem1);
 
   item["bananaNum1"] = new Game.Text("1", estiloNumeros);
@@ -84,12 +90,18 @@ function carregaCicloEcoli() {
   item.bananaSetaElem1Est.setZorder(7);
   itensCiclo.push(item.bananaSetaElem1Est);
 
-  item["bananaElem2a"] = new Game.Image("entamoebaCisto.png");
+  item["bananaElem2a"] = new Game.Item("entamoebaCisto.png");
   item.bananaElem2a.setPosition(200, 750);
   item.bananaElem2a.setScale(0.4);
   item.bananaElem2a.setZorder(6);
   //item.bananaElem2a.setAlpha(0);
   //item.bananaElem2a.disable();
+  item.bananaElem2a.onMouseOver = function() {
+    scene[0].setSubtitle("Desencistamento de <i>Entamoeba coli</i>: cisto");
+  }
+  item.bananaElem2a.onMouseOut = function() {
+    scene[0].setSubtitle("");
+  }
   itensCiclo.push(item.bananaElem2a);
 
   item["bananaNum2"] = new Game.Text("2", estiloNumeros);
@@ -103,12 +115,18 @@ function carregaCicloEcoli() {
   item.bananaSetaElem2aElem2b.setZorder(7);
   itensCiclo.push(item.bananaSetaElem2aElem2b);
 
-  item["bananaElem2b"] = new Game.Image("entamoebaTrofozoito.png");
+  item["bananaElem2b"] = new Game.Item("entamoebaTrofozoito.png");
   item.bananaElem2b.setPosition(537, 750);
   item.bananaElem2b.setScale(0.53);
   item.bananaElem2b.setZorder(6);
   //item.bananaElem2b.setAlpha(0);
   //item.bananaElem2b.disable();
+  item.bananaElem2b.onMouseOver = function() {
+    scene[0].setSubtitle("Desencistamento de <i>E. coli</i>: trofozoíto");
+  }
+  item.bananaElem2b.onMouseOut = function() {
+    scene[0].setSubtitle("");
+  }
   itensCiclo.push(item.bananaElem2b);
 
   item["bananaSetaElem2bEst"] = new Game.Line(537, 750, 920, 800, 4);
@@ -121,12 +139,18 @@ function carregaCicloEcoli() {
   item.bananaSetaEstElem3a.setZorder(7);
   itensCiclo.push(item.bananaSetaEstElem3a);
 
-  item["bananaElem3a"] = new Game.Image("entamoebaTrofozoito.png");
+  item["bananaElem3a"] = new Game.Item("entamoebaTrofozoito.png");
   item.bananaElem3a.setPosition(1330, 250);
   item.bananaElem3a.setScale(0.42);
   item.bananaElem3a.setZorder(6);
   //item.bananaElem3a.setAlpha(0);
   //item.bananaElem3a.disable();
+  item.bananaElem3a.onMouseOver = function() {
+    scene[0].setSubtitle("Encistamento de <i>E. coli</i>: trofozoíto");
+  }
+  item.bananaElem3a.onMouseOut = function() {
+    scene[0].setSubtitle("");
+  }
   itensCiclo.push(item.bananaElem3a);
 
   item["bananaNum3"] = new Game.Text("3", estiloNumeros);
@@ -140,12 +164,18 @@ function carregaCicloEcoli() {
   item.bananaSetaElem3aElem3b.setZorder(7);
   itensCiclo.push(item.bananaSetaElem3aElem3b);
 
-  item["bananaElem3b"] = new Game.Image("entamoebaCisto.png");
+  item["bananaElem3b"] = new Game.Item("entamoebaCisto.png");
   item.bananaElem3b.setPosition(1670, 250);
   item.bananaElem3b.setScale(0.55);
   item.bananaElem3b.setZorder(6);
   //item.bananaElem3b.setAlpha(0);
   //item.bananaElem3b.disable();
+  item.bananaElem3b.onMouseOver = function() {
+    scene[0].setSubtitle("Encistamento de <i>E. coli</i>: cisto");
+  }
+  item.bananaElem3b.onMouseOut = function() {
+    scene[0].setSubtitle("");
+  }
   itensCiclo.push(item.bananaElem3b);
 
   item["bananaSetaEstElem4"] = new Game.Line(1000, 810, 1170, 800, 4);
@@ -159,12 +189,18 @@ function carregaCicloEcoli() {
   item.bananaNum4.setPosition(1450, 660);
   itensCiclo.push(item.bananaNum4);
 
-  item["bananaElem4"] = new Game.Image("fezesComMosca.png");
+  item["bananaElem4"] = new Game.Item("fezesComMosca.png");
   item.bananaElem4.setPosition(1330, 800);
   item.bananaElem4.setScale(0.51);
   item.bananaElem4.setZorder(6);
   //item.bananaElem4.setAlpha(0);
   //item.bananaElem4.disable();
+  item.bananaElem4.onMouseOver = function() {
+    scene[0].setSubtitle("Moscas sendo infectadas por fezes com <i>E. coli</i>");
+  }
+  item.bananaElem4.onMouseOut = function() {
+    scene[0].setSubtitle("");
+  }
   itensCiclo.push(item.bananaElem4);
 
   item["bananaSetaElem4Elem5"] = new Game.Line(1330, 800, 1700, 800, 4);
@@ -177,12 +213,18 @@ function carregaCicloEcoli() {
   item.bananaNum5.setPosition(1851, 671);
   itensCiclo.push(item.bananaNum5);
 
-  item["bananaElem5"] = new Game.Image("entamoebaCisto.png");
+  item["bananaElem5"] = new Game.Item("entamoebaCisto.png");
   item.bananaElem5.setPosition(1700, 800);
   item.bananaElem5.setScale(0.56);
   item.bananaElem5.setZorder(6);
   //item.bananaElem5.setAlpha(0);
   //item.bananaElem5.disable();
+  item.bananaElem5.onMouseOver = function() {
+    scene[0].setSubtitle("Cisto de <i>E. coli</i>");
+  }
+  item.bananaElem5.onMouseOut = function() {
+    scene[0].setSubtitle("");
+  }
   itensCiclo.push(item.bananaElem5);
 
   /*var itensSobreDoenca = [];
