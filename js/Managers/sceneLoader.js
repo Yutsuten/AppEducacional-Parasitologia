69a3ac7bb924041a2c09c7c04411d8e357a5d830
Mateus Etto <mateus.etto@gmail.com>
2017-02-24 21:48:17 -0300

    added a way to see load progress using console.log

--- js/Managers/sceneLoader.js
+++ js/Managers/sceneLoader.js
@@ -3,18 +3,24 @@
 for (var i = 0; i < gameImages.length; i++)
   gameImages[i] = "img/" + gameImages[i];
 
-// After loading all images, load scenes
-loader.add(gameImages).load(function() {
-  console.log("Loading " + sceneLoader.length + " scenes.");
-  for (var i = 0; i < sceneLoader.length; i++) {
-    console.log("Loading scene " + i);
-    if (sceneLoader[i] != null)
-      sceneLoader[i]();
-    else
-      console.log("ERROR: Failed to load scene " + i);
-  }
-  console.log("Finish");
+var onProgressCallback = function(progressInfo) {
+  console.log(progressInfo.progress.toFixed(1));
+}
 
-  // Initial Scene
-  setScene(2);
-});
+// After loading all images, load scenes
+loader.add(gameImages)
+  .on("progress", onProgressCallback)
+  .load(function() {
+    console.log("Loading " + sceneLoader.length + " scenes.");
+    for (var i = 0; i < sceneLoader.length; i++) {
+      console.log("Loading scene " + i);
+      if (sceneLoader[i] != null)
+        sceneLoader[i]();
+      else
+        console.log("ERROR: Failed to load scene " + i);
+    }
+    console.log("Finish");
+
+    // Initial Scene
+    setScene(2);
+  });
