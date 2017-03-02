e1def320030831b050fb4c9aa10a77b83df35d95
Mateus Etto <mateus.etto@gmail.com>
2017-03-01 22:23:11 -0300

    added a loading progress at beginning

--- js/Managers/sceneLoader.js
+++ js/Managers/sceneLoader.js
@@ -3,14 +3,30 @@
 for (var i = 0; i < gameImages.length; i++)
   gameImages[i] = "img/" + gameImages[i];
 
+// Loading progress text
+var progressScreenText = new Game.Text("Carregando: 0%", {
+    "default": {fontFamily: "Book Antiqua", fontSize: "40px",
+      //stroke: 'black', strokeThickness: 10,
+      dropShadow: true, dropShadowBlur: 10,
+      fill: 0xFFFFFF, align: "left"
+    }
+  });
+progressScreenText.setAnchor(0.5, 0.5);
+progressScreenText.setPosition(1920/2, 1080/2);
+
 var onProgressCallback = function(progressInfo) {
-  console.log(progressInfo.progress.toFixed(1));
+  // Update text
+  progressScreenText.text = "Carregando: " + progressInfo.progress.toFixed(1) + "%";
+  // Draw on screen
+  stage.addChild(progressScreenText);
+  renderer.render(stage);
 }
 
 // After loading all images, load scenes
 loader.add(gameImages)
   .on("progress", onProgressCallback)
   .load(function() {
+    progressScreenText = null; // Delete the loading object
     console.log("Loading " + sceneLoader.length + " scenes.");
     for (var i = 0; i < sceneLoader.length; i++) {
       console.log("Loading scene " + i);
