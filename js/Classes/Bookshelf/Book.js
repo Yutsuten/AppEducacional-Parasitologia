e71276578ddca94a5d934a2b578a77e1f187324a
Mateus Etto <mateus.etto@gmail.com>
2017-03-09 22:41:26 -0300

    created arrow for next page (not working yet)

--- js/Classes/Bookshelf/Book.js
+++ js/Classes/Bookshelf/Book.js
@@ -1,11 +1,21 @@
 'use strict';
 
 Game.Book = function(name) {
-  var bookName = name;
-  var currentPageIndex = 0;
   var item = scene[2].item;
 
+  var bookName = name;
+  var currentPageIndex = 0;
+
   this.pagina = [];
+
+  item.bookNextPageArrow = new Game.Item("seta.png");
+  item.bookNextPageArrow.setPosition(1670, 500);
+  item.bookNextPageArrow.setBrightness(220);
+  item.bookNextPageArrow.setScale(0.3);
+  item.bookNextPageArrow.setAlpha(0);
+  item.bookNextPageArrow.setInteractive(false);
+  item.bookNextPageArrow.disable();
+  item.bookNextPageArrow.setZorder(2);
 
   var getItemName = function() {
     return "livro" + bookName + "Pagina" + currentPageIndex;
@@ -20,10 +30,20 @@ Game.Book = function(name) {
   this.open = function() {
     currentPageIndex = 0;
     item[getItemName()].show();
+
+    item.bookNextPageArrow.enable();
+    item.bookNextPageArrow.changeAlpha(1, 600);
+    item.bookNextPageArrow.setInteractive(true);
   }
 
   this.close = function() {
     item[getItemName()].hide();
+
+    item.bookNextPageArrow.changeAlpha(0, 600);
+    item.bookNextPageArrow.setInteractive(false);
+    setTimeout(function() {
+      item.bookNextPageArrow.disable();
+    }, 650);
   }
 
   this.nextPage = function() {
