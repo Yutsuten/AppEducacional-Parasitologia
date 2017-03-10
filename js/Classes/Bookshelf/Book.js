e0252d980134e1080dca0a62c3ac6e66f23ad7dd
Mateus Etto <mateus.etto@gmail.com>
2017-03-09 22:44:42 -0300

    created arrow for previous page

--- js/Classes/Bookshelf/Book.js
+++ js/Classes/Bookshelf/Book.js
@@ -17,6 +17,16 @@ Game.Book = function(name) {
   item.bookNextPageArrow.disable();
   item.bookNextPageArrow.setZorder(2);
 
+  item.bookPreviousPageArrow = new Game.Item("seta.png");
+  item.bookPreviousPageArrow.setPosition(110, 500);
+  item.bookPreviousPageArrow.setBrightness(220);
+  item.bookPreviousPageArrow.setScale(0.3);
+  item.bookPreviousPageArrow.setRotation(180);
+  item.bookPreviousPageArrow.setAlpha(0);
+  item.bookPreviousPageArrow.setInteractive(false);
+  item.bookPreviousPageArrow.disable();
+  item.bookPreviousPageArrow.setZorder(2);
+
   var getItemName = function() {
     return "livro" + bookName + "Pagina" + currentPageIndex;
   }
@@ -34,6 +44,9 @@ Game.Book = function(name) {
     item.bookNextPageArrow.enable();
     item.bookNextPageArrow.changeAlpha(1, 600);
     item.bookNextPageArrow.setInteractive(true);
+    item.bookPreviousPageArrow.enable();
+    item.bookPreviousPageArrow.changeAlpha(1, 600);
+    item.bookPreviousPageArrow.setInteractive(true);
   }
 
   this.close = function() {
@@ -41,8 +54,11 @@ Game.Book = function(name) {
 
     item.bookNextPageArrow.changeAlpha(0, 600);
     item.bookNextPageArrow.setInteractive(false);
+    item.bookPreviousPageArrow.changeAlpha(0, 600);
+    item.bookPreviousPageArrow.setInteractive(false);
     setTimeout(function() {
       item.bookNextPageArrow.disable();
+      item.bookPreviousPageArrow.disable();
     }, 650);
   }
 
